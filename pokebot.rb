80b12e56789d348b595cc424e8354b7b4c939f7b
Benjamin Manns <benmanns@gmail.com>
2013-08-19 16:57:28 -0400

    Reload on exception.

--- pokebot.rb
+++ pokebot.rb
@@ -11,65 +11,73 @@ require 'sinatra'
 redis = Redis.new(uri: ENV[ENV['REDIS_PROVIDER']])
 
 Thread.new do
-  agent = Mechanize.new
+  begin
+    agent = Mechanize.new
 
-  cookies = redis.get('cookies')
-  if cookies
-    Tempfile.open(['cookies', '.yml']) do |file|
-      file.write(cookies)
-      file.rewind
-      agent.cookie_jar.load(file.path)
-    end
-  else
-    page = agent.get('https://m.facebook.com/')
+    cookies = redis.get('cookies')
+    if cookies
+      Tempfile.open(['cookies', '.yml']) do |file|
+        file.write(cookies)
+        file.rewind
+        agent.cookie_jar.load(file.path)
+      end
+    else
+      page = agent.get('https://m.facebook.com/')
 
-    page = page.form_with(id: 'login_form').tap do |form|
-      form.email = ENV['FACEBOOK_EMAIL']
-      form.pass = ENV['FACEBOOK_PASS']
-    end.submit
+      page = page.form_with(id: 'login_form').tap do |form|
+        form.email = ENV['FACEBOOK_EMAIL']
+        form.pass = ENV['FACEBOOK_PASS']
+      end.submit
 
-    form = page.form_with(class: 'checkpoint')
-    if form
-      page = form.submit(form.button_with(name: 'submit[Continue]'))
-    end
-
-    form = page.form_with(class: 'checkpoint')
-    if form
-      page = form.submit(form.button_with(name: 'submit[This is Okay]'))
-    end
-
-    2.times do
       form = page.form_with(class: 'checkpoint')
       if form
-        form.radiobutton_with(name: 'name_action_selected', value: 'dont_save').check
         page = form.submit(form.button_with(name: 'submit[Continue]'))
       end
+
+      form = page.form_with(class: 'checkpoint')
+      if form
+        page = form.submit(form.button_with(name: 'submit[This is Okay]'))
+      end
+
+      2.times do
+        form = page.form_with(class: 'checkpoint')
+        if form
+          form.radiobutton_with(name: 'name_action_selected', value: 'dont_save').check
+          page = form.submit(form.button_with(name: 'submit[Continue]'))
+        end
+      end
+
+      cookies = Tempfile.open(['cookies', '.yml']) do |file|
+        agent.cookie_jar.save(file.path, session: true)
+        file.rewind
+        file.read
+      end
+
+      redis.set('cookies', cookies)
     end
 
-    cookies = Tempfile.open(['cookies', '.yml']) do |file|
-      agent.cookie_jar.save(file.path, session: true)
-      file.rewind
-      file.read
+    loop do
+      page = agent.get('https://m.facebook.com/pokes')
+      page.search('#root .poke').each do |poke|
+        poke_back = poke.search('a[href^="/a/notifications.php?poke="]').first
+        poke_back_href = poke_back.attributes['href'].value
+        poker_name = poke.search('.pokerName').first.text
+        agent.get(poke_back_href)
+        id = poker_name.parameterize
+        redis.sadd('pokers', id)
+        redis.set("poker:#{id}:name", poker_name)
+        times = redis.incr("poker:#{id}:times")
+        $stdout.puts "Poked #{poker_name} #{times} times."
+        $stdout.flush
+      end
+      sleep (ENV['INTERVAL'].to_f || 5.0)
     end
-
-    redis.set('cookies', cookies)
-  end
-
-  loop do
-    page = agent.get('https://m.facebook.com/pokes')
-    page.search('#root .poke').each do |poke|
-      poke_back = poke.search('a[href^="/a/notifications.php?poke="]').first
-      poke_back_href = poke_back.attributes['href'].value
-      poker_name = poke.search('.pokerName').first.text
-      agent.get(poke_back_href)
-      id = poker_name.parameterize
-      redis.sadd('pokers', id)
-      redis.set("poker:#{id}:name", poker_name)
-      times = redis.incr("poker:#{id}:times")
-      $stdout.puts "Poked #{poker_name} #{times} times."
-      $stdout.flush
-    end
-    sleep (ENV['INTERVAL'].to_f || 5.0)
+  rescue => e
+    $stderr.puts e
+    $stderr.flush
+    redis.del('cookies')
+    sleep 30
+    retry
   end
 end
 
