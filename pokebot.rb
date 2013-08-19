5d3e3b2236949799619ad77c7dd61c09fdf8a172
Benjamin Manns <benmanns@gmail.com>
2013-08-19 15:23:09 -0400

    Move entire Facebook initialization into thread.

--- pokebot.rb
+++ pokebot.rb
@@ -3,34 +3,34 @@ require 'sinatra'
 
 pokes = Hash.new(0)
 
-agent = Mechanize.new
+Thread.new do
+  agent = Mechanize.new
 
-page = agent.get('https://m.facebook.com/')
+  page = agent.get('https://m.facebook.com/')
 
-page = page.form_with(id: 'login_form').tap do |form|
-  form.email = ENV['FACEBOOK_EMAIL']
-  form.pass = ENV['FACEBOOK_PASS']
-end.submit
+  page = page.form_with(id: 'login_form').tap do |form|
+    form.email = ENV['FACEBOOK_EMAIL']
+    form.pass = ENV['FACEBOOK_PASS']
+  end.submit
 
-form = page.form_with(class: 'checkpoint')
-if form
-  page = form.submit(form.button_with(name: 'submit[Continue]'))
-end
-
-form = page.form_with(class: 'checkpoint')
-if form
-  page = form.submit(form.button_with(name: 'submit[This is Okay]'))
-end
-
-2.times do
   form = page.form_with(class: 'checkpoint')
   if form
-    form.radiobutton_with(name: 'name_action_selected', value: 'dont_save').check
     page = form.submit(form.button_with(name: 'submit[Continue]'))
   end
-end
 
-Thread.new do
+  form = page.form_with(class: 'checkpoint')
+  if form
+    page = form.submit(form.button_with(name: 'submit[This is Okay]'))
+  end
+
+  2.times do
+    form = page.form_with(class: 'checkpoint')
+    if form
+      form.radiobutton_with(name: 'name_action_selected', value: 'dont_save').check
+      page = form.submit(form.button_with(name: 'submit[Continue]'))
+    end
+  end
+
   loop do
     page = agent.get('https://m.facebook.com/pokes')
     page.search('#root .poke').each do |poke|
